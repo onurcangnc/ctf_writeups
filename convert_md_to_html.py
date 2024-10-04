83e2bdd166587dfe1ce4ac32d6f25057903b8111
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-10-05 02:19:58 +0300

    only Trace TryHackMe directory and just look md files.

--- convert_md_to_html.py
+++ convert_md_to_html.py
@@ -1,13 +1,17 @@
 import markdown2
 import os
 
-# Find all markdown files then create HTML with all of them.
-for file in os.listdir('.'):
-    if file.endswith('.md'):
-        html_file = f"{file[:-3]}.html"
-        with open(file, 'r', encoding='utf-8') as f:
-            markdown_content = f.read()
-        html_content = markdown2.markdown(markdown_content)
-        with open(html_file, 'w', encoding='utf-8') as f:
-            f.write(html_content)
-        print(f'{html_file} başarıyla güncellendi.')
+# TryHackMe klasörü içindeki alt dizinlerdeki .md dosyalarını bul ve her biri için HTML dosyası oluştur
+base_dir = './TryHackMe'
+
+for root, dirs, files in os.walk(base_dir):
+    for file in files:
+        if file.endswith('.md'):
+            md_file = os.path.join(root, file)
+            html_file = os.path.join(root, f"{file[:-3]}.html")  # .md'yi .html ile değiştir
+            with open(md_file, 'r', encoding='utf-8') as f:
+                markdown_content = f.read()
+            html_content = markdown2.markdown(markdown_content)
+            with open(html_file, 'w', encoding='utf-8') as f:
+                f.write(html_content)
+            print(f'{html_file} başarıyla oluşturuldu.')
