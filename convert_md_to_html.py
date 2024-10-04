4cdcaa243deecdef9eac12d11948447dab70d96d
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-10-05 02:24:32 +0300

    omit readme.md and embed images.

--- convert_md_to_html.py
+++ convert_md_to_html.py
@@ -6,12 +6,17 @@ base_dir = './TryHackMe'
 
 for root, dirs, files in os.walk(base_dir):
     for file in files:
-        if file.endswith('.md'):
+        if file.endswith('.md') and file != "README.md":  # README.md'yi dışla
             md_file = os.path.join(root, file)
             html_file = os.path.join(root, f"{file[:-3]}.html")  # .md'yi .html ile değiştir
             with open(md_file, 'r', encoding='utf-8') as f:
                 markdown_content = f.read()
+
+            # Markdown dosyasını HTML'ye çevir
             html_content = markdown2.markdown(markdown_content)
+
+            # HTML dosyasını yaz
             with open(html_file, 'w', encoding='utf-8') as f:
                 f.write(html_content)
+
             print(f'{html_file} başarıyla oluşturuldu.')
